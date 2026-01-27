c80a147880db127589a2c6ee46e9dde2685e652f
skxt <schinez90@gmail.com>
2026-01-27 23:16:08 +0100

    feat(Types): :label: update types

--- types.ts
+++ types.ts
@@ -1,27 +1,41 @@
 import { LucideIcon } from "lucide-react";
 
-export interface IFeature {
-    icon: LucideIcon;
-    title: string;
-    description: string;
-};
+export interface IWhere {
+  icon: LucideIcon;
+  title: string;
+  description: string;
+  cardBg: string;
+  iconBg: string;
+}
 
 export interface IWork {
-    title: string;
-    image: string;
+  title: string;
+  image: string;
 }
 
 export interface ITestimonial {
-    id: number;
-    name: string;
-    title: string;
-    quote: string;
-    avatar: string;
-    handle: string;
-    rating: 1 | 2 | 3 | 4 | 5;
+  id: number;
+  name: string;
+  title: string;
+  quote: string;
+  avatar: string;
+  handle: string;
+  rating: 1 | 2 | 3 | 4 | 5;
 }
 
 export interface IFaq {
-    question: string;
-    answer: string;
+  question: string;
+  answer: string;
+}
+
+export interface ISectionTitleAlt {
+  icon: LucideIcon;
+  title: string;
+  subtitle: string;
+  dir?: "left" | "center";
+}
+
+export interface ICustomIcon {
+  icon: LucideIcon;
+  dir?: "left" | "right";
 }
