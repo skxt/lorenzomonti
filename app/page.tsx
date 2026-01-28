3f5f98a5d590499a166529ea7ae5ddf024bd1927
Stefano Schinetti <stefano.schinetti@velve.it>
2026-01-28 12:40:09 +0100

    feat(Components): :sparkles: add new component CardExperience

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ import { TestimonialSection } from "@/sections/testimonial-section";
 import { WorkSection } from "@/sections/work-section";
 import { AboutSection } from "@/sections/about-section";
 import { WhereSection } from "@/sections/where-section";
+import { ExperienceSection } from "@/sections/experience-section";
 
 export default function Page() {
   return (
     <main className="mx-4 md:mx-16 lg:mx-24 xl:mx-32 border-x border-blue-100">
       <AboutSection />
-      <HeroSection />
-      <LogoMarquee />
+      {/* <LogoMarquee /> */}
+      <ExperienceSection />
       <FeatureSection />
       <WorkSection />
       <TestimonialSection />
