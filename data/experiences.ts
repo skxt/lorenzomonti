22a185c27f3ac198b35dc4545a05fc09e4d0ee92
skxt <schinez90@gmail.com>
2026-01-28 23:57:49 +0100

    feat(Data): add experience, change from testimonials to service

---
+++ data/experiences.ts
@@ -0,0 +1,44 @@
+import { IExperience } from "@/types";
+
+export const experiences: IExperience[] = [
+  {
+    image: "/logo-mark.svg",
+    title: "Consulente Tecnico d'Ufficio (CTU)",
+    company: "Tribunali Italiani",
+    location: "Italia",
+    start: "2020",
+    end: "Presente",
+    description: [
+      "Svolge l'incarico di Consulente Tecnico d'Ufficio presso diversi tribunali italiani.",
+      "Applica competenze in Medicina Legale e Assicurativa acquisite attraverso Master di II livello.",
+      "Particolare interesse per le Scienze Forensi e Criminologia Clinica.",
+    ],
+  },
+  {
+    image: "/logo-mark.svg",
+    title: "Specializzazione in Ortopedia e Traumatologia",
+    company: "Istituto Ortopedico Galeazzi",
+    location: "Milano",
+    start: "2009",
+    end: "2015",
+    description: [
+      "Consegue la Specializzazione in Ortopedia e Traumatologia con lode (70/70 e lode) presso l'Università degli Studi di Milano.",
+      "Approfondisce competenze in Traumatologia, Chirurgia del Ginocchio, Chirurgia della Mano, Chirurgia Ricostruttiva e delle Infezioni Osteo-Articolari.",
+      "Collabora con il reparto di Chirurgia d'Urgenza dell'Ospedale di Rho.",
+      "Consegue nel 2016 il diploma SIUMB in ecografia muscolo-scheletrica.",
+    ],
+  },
+  {
+    image: "/logo-mark.svg",
+    title: "Formazione in Chirurgia della Mano e Microchirurgia",
+    company: "Prince of Wales Hospital",
+    location: "Hong Kong",
+    start: "2016",
+    end: "2016",
+    description: [
+      "Periodo di formazione all'estero in Chirurgia della Mano, Microchirurgia e Chirurgia della Mano Pediatrica.",
+      "Perfezionamento in tecniche infiltrative eco-guidate.",
+      "Successivamente diventa docente in corsi di formazione per tecniche infiltrative eco-guidate.",
+    ],
+  },
+];
